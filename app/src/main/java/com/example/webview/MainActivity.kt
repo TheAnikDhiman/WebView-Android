56f713848067974b3ea63bbe457d9eb86273d6f9
Anik Dhiman <dhimananik2629@gmail.com>
2024-09-13 23:46:51 +0530

    Adding Files of Project WebView

---
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.example.webview
+
+import android.os.Build
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webViewVariable=findViewById<WebView>(R.id.webview)
+        webViewSetup(webViewVariable)
+        }
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun webViewSetup(webView: WebView){
+        webView.webViewClient = WebViewClient()
+        webView.apply {
+            settings.javaScriptEnabled = true
+            settings.safeBrowsingEnabled = true
+            loadUrl("https://github.com/TheAnikDhiman")
+        }
+    }
+}
